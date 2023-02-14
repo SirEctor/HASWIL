1ebf36219eb12c998d2709d41e653e7ae16352ac
SirEctor <ebenezer.teshome@duke.edu>
2023-02-14 16:59:45 -0500

    moved over to react fully

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>HASWIL: Here Are Some Words I Like</h1>
+        <h1>Word Stats</h1>
+        <h1>FAQ</h1>
+
       </header>
     </div>
   );
